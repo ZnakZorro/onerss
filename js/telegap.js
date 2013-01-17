8e5945e5cf71560fa55a73463f0d61e9ddb4247f
ZnakZorro <znakzorro@gmail.com>
2013-01-17 09:27:57 +0100

    add fix

--- js/telegap.js
+++ js/telegap.js
@@ -1,4 +1,20 @@
 /*
+to bardzo ciekawe do sprawdzenia
+var Anchors = document.getElementsByTagName("a");
+
+for (var i = 0; i < Anchors.length ; i++) {
+    Anchors[i].addEventListener("click", 
+        function (event) {
+            event.preventDefault();
+            if (confirm('Are you sure?')) {
+                window.location = this.href;
+            }
+        }, 
+        false);
+}
+*/
+
+/*
 function korekta_link(link,content)
 function szukaj_img(link,content)
 */
